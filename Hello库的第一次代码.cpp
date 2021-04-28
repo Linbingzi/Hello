1311de7d173db0b7d93848736d05271c05a8223a
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-04-28 13:19:52 +0800

    Add files via upload

---
+++ Hello库的第一次代码.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main()
+{
+	double a, b, c;
+	scanf("%lf %lf %lf", &a,&b,&c);
+	char d[] = "Hello,World! This is my begining!";
+	char e[] = " I am coming! My love!";
+	/*double d = sqrt(a);
+	printf("%lf", d);
+	return 0;*/
+	if ((sqrt(a) + sqrt(b)) == sqrt(c))
+	{
+		printf("%lf\t", (sqrt(a) + sqrt(b)));
+		printf("%lf\t", sqrt(c));
+		/*printf(" Hello,World! This is my begining!\n");*/
+		/*printf("%s\n", d);*/
+		puts(d);
+		/*printf("\n");*/
+	}
+	else
+	{
+		printf("%lf\t", (sqrt(a) + sqrt(b)));
+		printf("%lf\t", sqrt(c));
+		/*printf(" I am coming! My love!\n");*/
+		/*printf("%s\n", e);*/
+		puts(e);
+		/*printf("\n");*/
+	}
+	double f;
+	scanf("%lf", &f);
+	if (f == 521.1314)
+	{
+		printf("TRUE\n");
+	}
+	else
+	{
+		printf("FALSE\n");
+	}
+	system("pause");
+	return 0;
+}
